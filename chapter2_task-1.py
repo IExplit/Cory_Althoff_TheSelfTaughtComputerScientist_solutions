dc2e5134766c585f030dc418283b66d5321846c9
IExplit <124705005+IExplit@users.noreply.github.com>
2023-08-26 21:43:18 +0500

    Update chapter2_task-1.py

--- chapter2_task-1.py
+++ chapter2_task-1.py
@@ -1,5 +1,5 @@
 # Print the numbers from 1 to 10 recursively.
-
+# Cory Althoff's solution: https://github.com/calthoff/tstcs_challenge_solutions/blob/main/chapter2_ex1.py
 
 def counter(start = 1, end = 10):
     print(start)
