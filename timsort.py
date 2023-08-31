07d3371fe8c69f06766eeb81f2ece1251fc4edc1
IExplit <124705005+IExplit@users.noreply.github.com>
2023-08-31 17:14:35 +0500

    Add files via upload

---
+++ timsort.py
@@ -0,0 +1,102 @@
+import bisect
+from random import randint
+
+def get_minrun(n):
+    r = 0
+    while n >= 64:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+def insertion_sort(a_list, start, end):
+    for i in range(start+1, end+1):
+        
+        num = i
+        if i < len(a_list):
+            while a_list[i-1] > a_list[num] and i != start:
+                i -= 1
+            a_list.insert(i, a_list.pop(num))
+        else:
+            break
+        
+    return a_list
+
+def merge(a_list, start1, stop1, start2, stop2):
+    left_list = a_list[start1:stop1+start1].copy()
+    right_list = a_list[start2:stop2+start2].copy()
+    """print(len(right_list))
+    print(left_list, right_list, sep='\n')"""
+    main_id = start1
+    left_id = 0
+    right_id = 0
+    while left_id < len(left_list) and right_id < len(right_list) and main_id < len(a_list):
+        """print(len(left_list), len(right_list))"""
+        if left_list[left_id] <= right_list[right_id]:
+            a_list[main_id] = left_list[left_id]
+            left_id += 1
+        else:
+            a_list[main_id] = right_list[right_id]
+            right_id += 1
+        main_id += 1
+    """print(main_id, left_id, right_id)
+    print(len(a_list),len(left_list), len(right_list))
+    print(len(a_list[start1:stop2+start1]), left_list, right_list, sep='\n')"""
+    
+    while left_id < len(left_list) and main_id < len(a_list):
+        a_list[main_id] = left_list[left_id]
+        left_id += 1
+        main_id += 1
+    while right_id < len(right_list) and main_id < len(a_list):
+        a_list[main_id] = right_list[right_id]
+        right_id += 1
+        main_id += 1
+    return a_list
+
+
+def timsort(a_list, y2):
+    minrun = get_minrun(len(a_list))
+    runs = []
+
+    for i in range(0, len(a_list), minrun):
+        a_list = insertion_sort(a_list, i, minrun+i)
+        runs.append([i, len(a_list[i:minrun+i])])
+    
+    runs_id = len(runs) - 1
+    i = 1
+    while len(runs) > 1:
+        if len(runs) > 2:
+            if runs[i-1][1] <= runs[i+1][1]:
+                a_list = merge(a_list, runs[i-1][0], runs[i-1][1], runs[i][0], runs[i][1])
+                runs[i-1] = [runs[i-1][0], runs[i-1][1] + runs[i][1]]
+                runs.pop(i)
+
+            elif runs[i-1][1] > runs[i+1][1]:
+                a_list = merge(a_list, runs[i][0], runs[i][1], runs[i+1][0], runs[i+1][1])
+                runs[i] = [runs[i][0], runs[i+1][1] + runs[i][1]]
+                runs.pop(i+1)
+        else:
+            a_list = merge(a_list, runs[i-1][0], runs[i-1][1], runs[i][0], runs[i][1])
+            runs[i-1] = [runs[i-1][0], runs[i-1][0] + runs[i][1]]
+            runs.pop(i)
+        
+        if i >= len(runs) - 2:
+            i = 1
+        
+        else:
+            i += 1
+    return a_list == y2
+
+       
+
+cnt = 0
+for i in range(10000):
+    t = []
+    for i in range(randint(2, 10000)):
+        t.append(randint(0, 1000000000000000))
+    if timsort(t, sorted(t)) == False:
+        print('looool')
+        continue
+    cnt+=1
+print(cnt/1000*100)
+
+        
